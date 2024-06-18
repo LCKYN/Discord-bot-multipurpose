96b77cbc1a8b4750df4e1d3b1964480b1c616728
LCKYN <T.Pawarit@lckyn.com>
2024-06-18 09:28:52 +0700

    Add ignore channels for spam detection with link

--- extensions/spam_detector_with_link.py
+++ extensions/spam_detector_with_link.py
@@ -3,6 +3,8 @@ from interactions import Extension, listen
 from src.contains_link import contains_link
 from src.spam_detector import SpamDetector
 
+IGNORE_CHANNELS = [940952038991347722, 940966263168065547, 940918622845550612, 980657221845274654, 997121903493914665]
+
 
 class SpamDetectorWithLink(Extension):
 
@@ -20,6 +22,10 @@ class SpamDetectorWithLink(Extension):
         channel_id = event.message.channel.id
         content = event.message.content
         is_link = contains_link(content)
+        is_ignore_channel = channel_id in IGNORE_CHANNELS
+
+        if is_ignore_channel:
+            return
 
         if is_link:
             spam_result = self.spam_detector.add_message(user_id, content, channel_id)
@@ -28,7 +34,7 @@ class SpamDetectorWithLink(Extension):
 
     async def ban_spammer(self, user_id, channel_id, content):
         channel = await self.client.fetch_channel(channel_id)
-        await channel.send(f"ส่งไรจ๊ะ {user_id} แตก!")
+        await channel.send(f"ส่งไรจ๊ะ <@{user_id}> แตก!")
         guild = await self.client.fetch_guild(channel.guild.id)
         await guild.ban(
             user_id,
