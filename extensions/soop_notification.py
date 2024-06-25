0ab6b015e91401931b8ee92213d0041107456642
LCKYN <T.Pawarit@lckyn.com>
2024-06-25 11:31:36 +0700

    refactor: Update SoopNotification to fetch and send live stream notifications

--- extensions/soop_notification.py
+++ extensions/soop_notification.py
@@ -1,17 +1,18 @@
 import asyncio
-import json
 import os
 import time
+from datetime import datetime
 
-import pandas as pd
+import pytz
 import requests
 from dotenv import load_dotenv
 from interactions import Button, ButtonStyle, Embed, Extension, listen
 
+# Load environment variables
 load_dotenv()
 SOOP_TOKEN = os.getenv("SOOP_TOKEN")
-GUILD_ID = 1248571511061876838
 SOOP_CLIENT_ID = os.getenv("SOOP_CLIENT_ID")
+FIFATARGREAN_NOTIFICATION_CHANNEL = int(os.getenv("FIFATARGREAN_NOTIFICATION_CHANNEL"))
 
 
 class SoopNotification(Extension):
@@ -21,9 +22,8 @@ class SoopNotification(Extension):
         self.notification_task = None
         self.username_mapping = {
             "fifatargrean": {
-                "target_channel": 1252207765963669545,
-                "last_live_status": False,
-                "last_send_notification": 0,
+                "target_channel": FIFATARGREAN_NOTIFICATION_CHANNEL,
+                "last_send_notification": time.time(),
             },
         }
 
@@ -33,52 +33,84 @@ class SoopNotification(Extension):
         if self.notification_task is None:
             self.notification_task = asyncio.create_task(self.schedule_notifications())
 
-    async def get_soop_live_data(self):
-        URL = "https://api.sooplive.com/main/follow"
-        HEADERS = {
+    async def fetch_json(self, url, headers):
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+    async def get_stream_info(self, username):
+        headers = {
             "accept": "application/json",
-            "authorization": f"Bearer {SOOP_TOKEN}",
             "client-id": SOOP_CLIENT_ID,
         }
 
-        response = requests.request("GET", URL, headers=HEADERS)
-        response = json.loads(response.text)
-        response_pf = pd.DataFrame(response["getFollowChannelList"])
+        stream_info_url = f"https://api.sooplive.com/stream/info/{username}"
+        stream_info = await self.fetch_json(stream_info_url, headers)
+        return stream_info
 
-        for k, v in self.username_mapping.items():
+    async def get_stream_data(self, username, stream_data):
+        headers = {
+            "accept": "application/json",
+            "client-id": SOOP_CLIENT_ID,
+        }
 
-            filtered_df = response_pf[response_pf["channelId"] == k]
+        url_viewer = f"https://api.sooplive.com/stream/info/{username}/live"
+        url_info = f"https://api.sooplive.com/channel/info/{username}"
 
-            categories = filtered_df["streamCategories"].values[0][0]
-            current_status = filtered_df["isStreaming"].values[0]
-            display_name = filtered_df["nickname"]
-            thumbnail = filtered_df["thumbnailUrl"].values[0]
-            title = filtered_df["title"].values[0]
-            viewer_count = filtered_df["streamViewerCount"].values[0]
+        response_viewer = await self.fetch_json(url_viewer, headers)
+        response_info = await self.fetch_json(url_info, headers)
 
-            if not v["last_live_status"] and current_status and time.time() - v["last_send_notification"] > 1200:
-                channel = await self.bot.fetch_channel(v["target_channel"])
-                components = Button(
-                    style=ButtonStyle.LINK,
-                    label="Watch Stream",
-                    url=f"https://www.sooplive.com/{k}",
-                )
+        display_name = response_info["streamerChannelInfo"]["nickname"]
+        start_time = datetime.strptime(stream_data["streamStartDate"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        utc_zone = pytz.utc
+        thailand_zone = pytz.timezone("Asia/Bangkok")
 
-                await channel.send(
-                    content=f"@everyone {display_name.values[0]} is now live!",
-                    embed=Embed(color=0xD2FE2C)
-                    .add_field(
-                        name=f"{display_name.values[0]} is now live on Soop!",
-                        value=f"[{title}](https://www.sooplive.com/{k})",
-                    )
-                    .add_field(name="Categories", value=categories, inline=True)
-                    .add_field(name="Viewers", value=viewer_count, inline=True)
-                    .set_image(url=thumbnail),
-                    components=[components],
-                )
+        start_time = utc_zone.localize(start_time)
+        thailand_time = start_time.astimezone(thailand_zone)
 
-                self.username_mapping[k]["last_send_notification"] = time.time()
-            self.username_mapping[k]["last_live_status"] = filtered_df["isStreaming"]
+        return {
+            "display_name": display_name,
+            "category": "Just Chatting",  # Stream data category hardcoded as "Just Chatting"
+            "language": stream_data["languageCode"],
+            "start_time": thailand_time.timestamp(),
+            "thumbnail": f"https://api.sooplive.com/media/live/{username}/thumbnail.jpg",
+            "title": stream_data["title"],
+            "viewer_count": response_viewer["viewer"],
+        }
+
+    async def send_notification(self, username, stream_data):
+        target_channel = self.username_mapping[username]["target_channel"]
+        channel = await self.bot.fetch_channel(target_channel)
+
+        components = Button(
+            style=ButtonStyle.LINK,
+            label="Watch Stream",
+            url=f"https://www.sooplive.com/{username}",
+        )
+
+        await channel.send(
+            content=f"@everyone {stream_data['display_name']} is now live!",
+            embed=Embed(color=0xD2FE2C)
+            .add_field(
+                name=f"{stream_data['display_name']} is now live on Soop!",
+                value=f"[{stream_data['title']}](https://www.sooplive.com/{username})",
+            )
+            .add_field(name="Categories", value=stream_data["category"], inline=True)
+            .add_field(name="Viewers", value=stream_data["viewer_count"], inline=True)
+            .set_image(url=stream_data["thumbnail"]),
+            components=[components],
+        )
+
+    async def get_soop_live_data(self):
+        for username in self.username_mapping:
+            stream_info = await self.get_stream_info(username)
+            if stream_info["isStream"]:
+                stream_data = await self.get_stream_data(username, stream_info["data"])
+                print(self.username_mapping[username]["last_send_notification"] - stream_data["start_time"])
+                if self.username_mapping[username]["last_send_notification"] < stream_data["start_time"]:
+                    await self.send_notification(username, stream_data)
+                    self.username_mapping[username]["last_send_notification"] = time.time()
+
+            await asyncio.sleep(1)
 
     async def schedule_notifications(self):
         while True:
